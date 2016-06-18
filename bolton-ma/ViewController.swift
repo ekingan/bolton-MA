03425ef4875fd9ed2b42679a1c821a2abe3fc2af
ekingan <ekingan@gmail.com>
2016-06-18 14:24:25 -0700

    first page layed out

--- bolton-ma/ViewController.swift
+++ bolton-ma/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moreBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        moreBtn.layer.cornerRadius = 2.0
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
